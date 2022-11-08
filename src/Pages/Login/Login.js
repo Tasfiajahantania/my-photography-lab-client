f9e6729fafa8bdb2bda961578d99d02fb440976f
Tasfiajahan <tania.develpers@gmail.com>
2022-11-08 17:24:19 +0600

    my third commit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 
 const Login = () => {
+  const { login } = useContext(AuthContext);
   const handleLogin = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
+
+    login(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .then((error) => console.log(error));
   };
   return (
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20 mx-auto">
+    <div className="card flex-shrink-0 w-full mb-10 max-w-sm shadow-2xl bg-base-100 py-20 mx-auto">
       <h1 className="text-5xl text-center font-bold">Login</h1>
       <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
@@ -28,7 +37,7 @@ const Login = () => {
             <span className="label-text">Password</span>
           </label>
           <input
-            type="text"
+            type="password"
             name="password"
             placeholder="password"
             className="input input-bordered"
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 
 const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
   const handleSignUp = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => console.error(err));
   };
   return (
-    <div className="card flex-shrink-0 w-full max-w-sm mx-auto shadow-2xl bg-base-100 py-20">
+    <div className="card flex-shrink-0 w-full max-w-sm mx-auto mb-10 shadow-2xl bg-base-100 py-20">
       <h1 className="text-5xl text-center font-bold">Sign Up</h1>
       <form onSubmit={handleSignUp} className="card-body">
         <div className="form-control">
@@ -40,7 +49,7 @@ const SignUp = () => {
             <span className="label-text">Password</span>
           </label>
           <input
-            type="text"
+            type="password"
             name="password"
             placeholder="password"
             className="input input-bordered"
