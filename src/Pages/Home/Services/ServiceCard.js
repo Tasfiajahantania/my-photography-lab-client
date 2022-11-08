e2bc39a226c7ba7ae1077da328c0b4af6cad5c73
Tasfiajahan <tania.develpers@gmail.com>
2022-11-08 14:25:35 +0600

    first commit

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  const { _id, img, price, title, description } = service;
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl mb-8">
+      <figure>
+        <img src={img} alt="Img" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>Price: ${price}</p>
+        <p></p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+  return (
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20 mx-auto">
+      <h1 className="text-5xl text-center font-bold">Login</h1>
+      <form onSubmit={handleLogin} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            className="input input-bordered"
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <input className="btn btn-primary" type="submit" value="Login" />
+        </div>
+      </form>
+      <p className="text-center">
+        New to Genius Car{" "}
+        <Link className="text-blue-600 font-bold" to="/signup">
+          Sign Up
+        </Link>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Login;
